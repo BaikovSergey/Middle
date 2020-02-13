836751363cb1228ba4de92283e35bbf568a9e29c
Sergey Baikov <rayleonard9@gmail.com>
2020-02-13 17:16:03 +0300

    Реализовать шаблон Producer Consumer.

---
+++ Multithreading/src/main/java/ru/job4j/pingpong/producerconsumer/SimpleBlockingQueue.java
@@ -0,0 +1,27 @@
+package ru.job4j.pingpong.producerconsumer;
+
+import javax.annotation.concurrent.ThreadSafe;
+import javax.annotation.concurrent.GuardedBy;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    public synchronized void offer(T value) {
+        this.queue.offer(value);
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        T result = null;
+        if (this.queue.isEmpty()) {
+            Thread.currentThread().wait();
+        } else {
+            result = this.queue.poll();
+        }
+        return result;
+    }
+}
---
+++ Multithreading/src/test/java/ru/job4j/pingpong/producerconsumer/SimpleBlockingQueueTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.pingpong.producerconsumer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void test() {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+
+    }
+
+}

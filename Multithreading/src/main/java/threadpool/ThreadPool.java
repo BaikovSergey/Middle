575458af0d2b0b686c01e82ded98fbe112c1a8ea
Sergey Baikov <rayleonard9@gmail.com>
2020-02-18 16:33:03 +0300

    ThreadSafe Singleton

---
+++ Multithreading/src/main/java/threadpool/ThreadPool.java
@@ -0,0 +1,20 @@
+package threadpool;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+
+    int size = Runtime.getRuntime().availableProcessors();
+
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+
+    public void work(Runnable job) {
+
+    }
+
+    public void shutdown() {
+
+    }
+}

f96e387ee34013d73002ba8934de3bc75b2b5f2d
Sergey Baikov <rayleonard9@gmail.com>
2020-03-03 11:26:44 +0300

    1. Реализовать шаблон Producer Consumer

--- Multithreading/src/main/java/ru/job4j/pingpong/producerconsumer/SimpleBlockingQueue.java
+++ Multithreading/src/main/java/ru/job4j/pingpong/producerconsumer/SimpleBlockingQueue.java
@@ -4,30 +4,48 @@ import javax.annotation.concurrent.ThreadSafe;
 import javax.annotation.concurrent.GuardedBy;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
 
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
 
-    private int maxSize;
+    private volatile int capacity;
 
     public SimpleBlockingQueue(int maxLimit) {
-        this.maxSize = maxLimit;
+        this.capacity = maxLimit;
     }
 
-    @GuardedBy("this")
-    private Queue<T> queue = new LinkedList<>();
+    private final Queue<T> queue = new LinkedList<>();
 
-    public synchronized void offer(T value) {
-        this.queue.offer(value);
-    }
-
-    public synchronized T poll() throws InterruptedException {
-        T result = null;
-        if (this.queue.isEmpty()) {
-            Thread.currentThread().wait();
-        } else {
-            result = this.queue.poll();
+    public void offer(T value) throws InterruptedException {
+        synchronized (this.queue) {
+            while (this.queue.size() == this.capacity) {
+                System.out.println("Producer" + Thread.currentThread().getName() + " is waiting...");
+                System.out.println(this.queue.size());
+                this.queue.wait(ThreadLocalRandom.current().nextInt(1000));
+                System.out.println("Producer" + Thread.currentThread().getName() + " is awakened");
+            }
+            this.queue.offer(value);
+            this.queue.notifyAll();
+            System.out.println("Offering item");
+            Thread.sleep(1000);
         }
-        return result;
+    }
+
+    public T poll() throws InterruptedException {
+        T result;
+        synchronized (this.queue) {
+            while (this.queue.size() == 0) {
+                System.out.println("Consumer " + Thread.currentThread().getName() + " is empty, waiting...");
+                System.out.println(this.queue.size());
+                this.queue.wait(ThreadLocalRandom.current().nextInt(1000));
+                System.out.println("Consumer " + Thread.currentThread().getName() + " is awakened");
+            }
+            result = this.queue.poll();
+            this.queue.notifyAll();
+            System.out.println("Item was taken");
+            Thread.sleep(1000);
+            return result;
+        }
     }
 }

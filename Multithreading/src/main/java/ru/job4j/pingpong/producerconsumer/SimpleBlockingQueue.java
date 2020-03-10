2d8581406239b51848a4d54a5d8a9a9b0e238ea5
Sergey Baikov <rayleonard9@gmail.com>
2020-03-10 09:58:43 +0300

    Реализовать шаблон Producer Consumer

--- Multithreading/src/main/java/ru/job4j/pingpong/producerconsumer/SimpleBlockingQueue.java
+++ Multithreading/src/main/java/ru/job4j/pingpong/producerconsumer/SimpleBlockingQueue.java
@@ -9,43 +9,30 @@ import java.util.concurrent.ThreadLocalRandom;
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
 
-    private volatile int capacity;
+    private final int capacity;
 
-    public SimpleBlockingQueue(int maxLimit) {
-        this.capacity = maxLimit;
-    }
-
+    @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
 
-    public void offer(T value) throws InterruptedException {
-        synchronized (this.queue) {
-            while (this.queue.size() == this.capacity) {
-                System.out.println("Producer" + Thread.currentThread().getName() + " is waiting...");
-                System.out.println(this.queue.size());
-                this.queue.wait(ThreadLocalRandom.current().nextInt(1000));
-                System.out.println("Producer" + Thread.currentThread().getName() + " is awakened");
-            }
-            this.queue.offer(value);
-            this.queue.notifyAll();
-            System.out.println("Offering item");
-            Thread.sleep(1000);
-        }
+    public SimpleBlockingQueue(int capacity) {
+        this.capacity = capacity;
     }
 
-    public T poll() throws InterruptedException {
-        T result;
-        synchronized (this.queue) {
-            while (this.queue.size() == 0) {
-                System.out.println("Consumer " + Thread.currentThread().getName() + " is empty, waiting...");
-                System.out.println(this.queue.size());
-                this.queue.wait(ThreadLocalRandom.current().nextInt(1000));
-                System.out.println("Consumer " + Thread.currentThread().getName() + " is awakened");
-            }
-            result = this.queue.poll();
-            this.queue.notifyAll();
-            System.out.println("Item was taken");
-            Thread.sleep(1000);
-            return result;
+    public synchronized void offer(T value) throws InterruptedException {
+        while (this.queue.size() == this.capacity) {
+            wait(ThreadLocalRandom.current().nextInt(1000));
         }
+        this.queue.offer(value);
+        notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        T result;
+        while (this.queue.size() == 0) {
+            wait(ThreadLocalRandom.current().nextInt(1000));
+        }
+        result = this.queue.poll();
+        notifyAll();
+        return result;
     }
 }
